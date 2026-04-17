2f1c8fd0994a2bf4ca6338a87f405d24d54e0849
Gajanandyadav1 <gajanandsabalpura06@gmail.com>
2026-04-17 11:29:39 +0530

    ss

--- src/components/website/DifferenceSection.jsx
+++ src/components/website/DifferenceSection.jsx
@@ -62,6 +62,7 @@ export default function DifferenceSection({ data }) {
                 }}
               >
                 {data.subheading}
+                
               </span>
             )}
 
