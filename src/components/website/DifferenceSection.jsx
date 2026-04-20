adcb55a7781bd5b6722dd6e6941f4e0813418acf
Gajanandyadav1 <gajanandsabalpura06@gmail.com>
2026-04-20 12:15:17 +0530

    changes color

--- src/components/website/DifferenceSection.jsx
+++ src/components/website/DifferenceSection.jsx
@@ -61,6 +61,7 @@ export default function DifferenceSection({ data }) {
                   color: primaryColor,
                 }}
               >
+                
                 {data.subheading}
                 
               </span>
